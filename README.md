fa196e3a44584b3e4e8f28c5921a19932c3234b9
jmvalick <jmvalick@gmail.com>
2025-02-24 21:27:27 -0500

    "added readme, image, new clapper file"

---
+++ README.md
@@ -0,0 +1,14 @@
+# Arduino Projects
+
+This is a repository of short Arduino projects I made for practice. My favorite and most involved project is under the [clap_on_clap_off](./clap_on_clap_off/) folder.
+
+## Clap On Clap Off Project
+- The goal of this project was to replace my family's old Clapper device that had always been used to control Christmas tree lights. The old clapper was failing so I decided to make a new one.
+
+- This device includes an Arduino Mini, re-wired power strip, a microphone, and a relay. The Arduino waits for 2 successive claps before toggling the relay, which in turn toggles the power strips outlets and the devices connected to it.
+
+- This project also required developing a 3d model to print and house the electrionics as well as soldering to rewire the power strip.
+
+
+<img src="./clap_on_clap_off/Results/20221217_192822.jpg" width="300"/>
+
--- clap_on_clap_off/clap_on_clap_off.ino
+++ clap_on_clap_off/clap_on_clap_off.ino
@@ -1,35 +1,61 @@
-int relayPin = 11;   //Relay IN1 connected to pin 7
-int micPin = A1;   // Mic connected to pin 6
-int micVal = 0;      //HIGH is no sound, LOW is sound
+//Pins
+int relayPin = 5;  
+int micPin = A5;   
+int micVal = 0;      
 
-boolean pwrToggle = true;
+//consts
+const int noiseThresh = 800;    //threshold of noise
+const int clapNum = 2;          //number of claps to change state
+const int maxClapTime = 2000;   //time allowed between claps
+
+//variables
+boolean pwrToggle = true;       //relay state
+int counter = 0;                //clap count
+float timer = 0.0;              //time after clap
 
 void setup ()
 {
-  Serial.begin(74880);  
-  pinMode(relayPin, OUTPUT);  //Set pin for output
-  //digitalWrite(relayPin, HIGH);  // HIGH is off, LOW is on
-  //pinMode (micPin, INPUT) ;   //Set pin for input
+  Serial.begin(230400);  
+  pinMode(relayPin, OUTPUT);     //Set relayPin for output
+  digitalWrite(relayPin, HIGH);  // HIGH is off, LOW is on
+  pinMode (micPin, INPUT) ;      //Set micPin for input
 }
-void loop ()
-{
-  micVal = analogRead(micPin) ; // Listen...
+void loop (){
   
-  if (micVal > 600) // Hear something!!!
-  {
-      if (pwrToggle == true) {      //relay off (true) Turn it on and display message in serial Monitor
-            Serial.println ("Relay on");
-            digitalWrite(relayPin, HIGH);
-            pwrToggle = false;
-            delay (500);
-      } else {    //relay on (true) Turn it off and display message in serial Monitor
-            Serial.println ("Relay off");
-            digitalWrite(relayPin, LOW);
-            pwrToggle = true;
-            delay (500);
-      }
+  micVal = analogRead (micPin);
+  
+  if (micVal > noiseThresh){  // Hear something
+
+    counter += 1;
+
+    if (pwrToggle == true && counter == clapNum) {  //relay off (true) Turn it on and display message in serial Monitor
+      Serial.println ("Relay on");
+      digitalWrite(relayPin, LOW);
+      pwrToggle = false;
+      counter = 0;
+    }
+    if (pwrToggle == false && counter == clapNum) { //relay on (false) Turn it off and display message in serial Monitor
+      Serial.println ("Relay off");
+      digitalWrite(relayPin, HIGH);
+      pwrToggle = true;
+      counter = 0;
+    }
+
+    else {  //first clap
+      timer = millis();
+    }
+    delay (500);
+  }  
+
+  if ((millis() - timer) > maxClapTime){  //if no second clap after maxTime reset counter and timer
+    counter = 0;
+    timer = millis();
   }
-  Serial.println (micVal);
-  delay(5);
+
+  //Serial.print(counter);
+  //Serial.print("  "); 
+  //Serial.print(timer);
+  //Serial.print("   ");
+  //Serial.println(micVal);
 }
  
